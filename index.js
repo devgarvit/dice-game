3f00def658694c87696f41cadd658fb8ccb64215
Rohit7364 <99162115+Rohit7364@users.noreply.github.com>
2022-04-13 12:33:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+var randomimageSource = "images/" + randomDiceImage;
+document.querySelectorAll("img")[0].setAttribute("src", randomimageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1; //1-6
+var randomDiceImage1 = "images/dice" + randomNumber2 + ".png";//dice1-dice6
+document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage1);//static to dymanic image
+
+if(randomNumber1>randomNumber2){
+  document.querySelector("h1").textContent="Player1 Win!!";
+}
+else if(randomNumber1<randomNumber2){
+  document.querySelector("h1").textContent="Player2 Win!!";
+}
+else{
+    document.querySelector("h1").textContent="Draw!!";
+
+}
